442c1a740440d7a8e0c9eabbd357cf8d22365166
Calebe Machado <calebe.machado@ifood.com.br>
2024-01-13 11:33:00 -0300

    Change customer database population to application so hibernate can handle auto increment correctly

--- src/main/java/com/example/api/ApiApplication.java
+++ src/main/java/com/example/api/ApiApplication.java
@@ -1,12 +1,34 @@
 package com.example.api;
 
+import com.example.api.domain.Customer;
+import com.example.api.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    private final CustomerRepository customerRepository;
+
+    public ApiApplication(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner populateDatabase() {
+        return args -> {
+            customerRepository.save(new Customer("HOMEM ARANHA", "aranha@vingadores.com", "M"));
+            customerRepository.save(new Customer("THOR", "thor@vingadores.com", "M"));
+            customerRepository.save(new Customer("VIUVA NEGRA", "viuva@vingadores.com", "F"));
+            customerRepository.save(new Customer("NAMOR", "namor@vingadores.com", "M"));
+            customerRepository.save(new Customer("GAMORA", "gamora@vingadores.com", "F"));
+            customerRepository.save(new Customer("Hulk", "hulk@vingadores.com", "m"));
+        };
+    }
 }
