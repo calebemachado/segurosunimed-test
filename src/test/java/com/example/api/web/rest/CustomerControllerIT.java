15a34812be2c39c0bbd5be413c6aae3ac8984f6d
Calebe Machado <calebe.machado@ifood.com.br>
2024-01-13 09:14:15 -0300

    Create controller integration test

---
+++ src/test/java/com/example/api/web/rest/CustomerControllerIT.java
@@ -0,0 +1,70 @@
+package com.example.api.web.rest;
+
+import com.example.api.domain.Customer;
+import com.example.api.service.CustomerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(CustomerController.class)
+public class CustomerControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CustomerService service;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testFindAll() throws Exception {
+        List<Customer> customers = Arrays
+                .asList(new Customer(1L, "John", "john@gmail.com", "male"),
+                        new Customer(2L, "Jane", "jane@gmail.com", "female"));
+        when(service.findAll()).thenReturn(customers);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/customers")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(customers)));
+    }
+
+    @Test
+    void testFindById() throws Exception {
+        Long customerId = 1L;
+        Customer customer = new Customer(1L, "John", "john@gmail.com", "male");
+        when(service.findById(customerId)).thenReturn(Optional.of(customer));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/customers/{id}", customerId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(customer)));
+    }
+
+    @Test
+    void testFindByIdNotFound() throws Exception {
+        Long customerId = 1L;
+        when(service.findById(customerId)).thenReturn(Optional.empty());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/customers/{id}", customerId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+}
